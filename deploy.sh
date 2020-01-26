d83e4dd37bae6bff6e59ecb17987f01a00949a9e
Caleb Ikhuohon <calebikhuohon@gmail.com>
2020-01-26 06:53:54 +0100

    chore: setup deploy scripts

---
+++ deploy.sh
@@ -0,0 +1,15 @@
+ docker build -t calebikhuohon/complesso-client:latest -t calebikhuohon/complesso-client:$GIT_SHA -f ./client/Dockerfile ./client
+ docker build -t calebikhuohon/complesso-server:latest -t calebikhuohon/complesso-server:$GIT_SHA -f ./client/Dockerfile ./server
+ docker build -t calebikhuohon/complesso-worker:latest -t calebikhuohon/complesso-worker:$GIT_SHA -f ./client/Dockerfile ./worker
+ docker push calebikhuohon/complesso-client:latest
+ docker push calebikhuohon/complesso-server:latest
+ docker push calebikhuohon/complesso-worker:latest
+
+ docker push calebikhuohon/complesso-client:$GIT_SHA
+ docker push calebikhuohon/complesso-server:$GIT_SHA
+ docker push calebikhuohon/complesso-worker:$GIT_SHA
+ 
+ kubectl apply -f k8s
+ kubectl set image deployments/server-deployment server=calebikhuohon/complesso-client:$GIT_SHA
+ kubectl set image deployments/client-deployment client=calebikhuohon/complesso-client:$GIT_SHA
+ kubectl set image deployments/worker-deployment worker=calebikhuohon/complesso-worker:$GIT_SHA
