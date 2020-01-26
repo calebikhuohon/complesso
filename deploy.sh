40133a93bffdd9fb43ce4890a9f934409c531274
Caleb Ikhuohon <calebikhuohon@gmail.com>
2020-01-26 07:18:09 +0100

    fix: ensure correct Dockefile locations

--- deploy.sh
+++ deploy.sh
@@ -1,6 +1,6 @@
  docker build -t calebikhuohon/complesso-client:latest -t calebikhuohon/complesso-client:$GIT_SHA -f ./client/Dockerfile ./client
- docker build -t calebikhuohon/complesso-server:latest -t calebikhuohon/complesso-server:$GIT_SHA -f ./client/Dockerfile ./server
- docker build -t calebikhuohon/complesso-worker:latest -t calebikhuohon/complesso-worker:$GIT_SHA -f ./client/Dockerfile ./worker
+ docker build -t calebikhuohon/complesso-server:latest -t calebikhuohon/complesso-server:$GIT_SHA -f ./server/Dockerfile ./server
+ docker build -t calebikhuohon/complesso-worker:latest -t calebikhuohon/complesso-worker:$GIT_SHA -f ./worker/Dockerfile ./worker
  docker push calebikhuohon/complesso-client:latest
  docker push calebikhuohon/complesso-server:latest
  docker push calebikhuohon/complesso-worker:latest
